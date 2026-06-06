d2cfe4f0c83d58c9b612045ee2f0552e28d097d5
Sachin Prabhu <12785520+sachinprabhu007@users.noreply.github.com>
2026-06-06 17:53:20 +0530

    testing for render

--- src/classifier.py
+++ src/classifier.py
@@ -1,5 +1,8 @@
-from transformers import pipeline
-import streamlit as st
+# from transformers import pipeline
+# import streamlit as st
+
+# print("classifier.py imported", flush=True)
+# print("transformers imported", flush=True)
 
 # Minimum confidence required to accept a prediction
 CONFIDENCE_THRESHOLD = 0.90
@@ -21,40 +24,48 @@ def get_classifier():
         model="chriamue/bird-species-classifier"
     )
 
-
 def detect_bird(image):
-    """
-    Detect the bird species from an input image.
-    """
-
-    classifier = get_classifier()
-
-    results = classifier(image)
-
-    # Store top predictions for display in the UI
-    top_predictions = [
-        (
-            pred["label"].upper(),
-            pred["score"]
-        )
-        for pred in results[:5]
-    ]
-
-    best = results[0]
-
-    species = best["label"].upper().strip()
-    confidence = float(best["score"])
-
-    # Reject low-confidence predictions
-    if confidence < CONFIDENCE_THRESHOLD:
-        return (
-            "Unknown Bird",
-            confidence,
-            top_predictions
-        )
-
     return (
-        species,
-        confidence,
-        top_predictions
+        "PEACOCK",
+        0.99,
+        [
+            ("PEACOCK", 0.99)
+        ]
     )
+
+# def detect_bird(image):
+#     """
+#     Detect the bird species from an input image.
+#     """
+
+#     classifier = get_classifier()
+
+#     results = classifier(image)
+
+#     # Store top predictions for display in the UI
+#     top_predictions = [
+#         (
+#             pred["label"].upper(),
+#             pred["score"]
+#         )
+#         for pred in results[:5]
+#     ]
+
+#     best = results[0]
+
+#     species = best["label"].upper().strip()
+#     confidence = float(best["score"])
+
+#     # Reject low-confidence predictions
+#     if confidence < CONFIDENCE_THRESHOLD:
+#         return (
+#             "Unknown Bird",
+#             confidence,
+#             top_predictions
+#         )
+
+#     return (
+#         species,
+#         confidence,
+#         top_predictions
+#     )
